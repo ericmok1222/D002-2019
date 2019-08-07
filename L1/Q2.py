2fd675452e795eb2784625f2d451f4ef3d0a9818
ericmok1222 <53845466+ericmok1222@users.noreply.github.com>
2019-08-07 11:59:48 +0800

    Add files via upload

---
+++ L1/Q2.py
@@ -0,0 +1,9 @@
+from math import *
+print(8*3.57)
+print(5+30*20)
+print((5+3)*20)
+print(1+(2+20*3)/(4*2))
+print(1+2**10)
+print(ceil(29.0/4))
+r=sqrt(100/pi)
+print((2*r+1)**2)
---
+++ L1/Q3.py
@@ -0,0 +1,4 @@
+from math import *
+r=float(input("Please input the radius of the circle\n"))
+a=(r**2*pi)
+print("The circle area is %.2f"%a)
