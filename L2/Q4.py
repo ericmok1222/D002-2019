c85a9d5608a2f3a4e6bbe062d667345e03748f2d
ericmok1222 <53845466+ericmok1222@users.noreply.github.com>
2019-08-12 10:40:25 +0800

    Add files via upload

--- L2/Q4.py
+++ L2/Q4.py
@@ -1,10 +1,12 @@
 max=0
-for a in range(1,60):
-    for b in range(1,60):
-        for c in range(1,60):
-            for d in range(1,60):
+for a in range(0,61):
+    for b in range(0,61):
+        for c in range(0,61):
+            for d in range(0,61):
                 if a+b+c+d==60:
                     t_max=a*b+a*c+c*d
                     if t_max>max:
                         max=t_max
+                        print(a,b,c,d)
 print(max)
+input()
