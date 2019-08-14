9ce0ce77c70c60835ddab91797d6698313f7a990
ericmok1222 <53845466+ericmok1222@users.noreply.github.com>
2019-08-14 12:42:01 +0800

    Add files via upload

--- L3/hangman.py
+++ L3/hangman.py
@@ -1,18 +1,25 @@
-import random
-answer_list=["apple","arrange","quiet","op","pop","cute"]
-x=random.randint(0,len(answer_list)-1)
-answer = answer_list[x]
-print(answer)
-print("there are %d letter in the word"%len(answer))
-show_answer=[]
-count=0
-t_count=0
-no="_"
-for i in range(0,len(answer)):
-    show_answer+=[no]
-print(show_answer)
-while True:
-    guess=input("guess the letter")
+def ran_answer(answer_list):
+    x=random.randint(0,len(answer_list)-1)
+    answer = answer_list[x]
+    return answer 
+def create_show_answer(answer):
+    show_answer=[]
+    no="_"
+    for i in range(0,len(answer)):
+        show_answer+=[no]
+    return(show_answer)
+def get_guess():
+    global guess_list
+    while True:
+        guess=input("guess the letter")
+        if guess in guess_list:
+            print("enter already")
+        else:
+            guess_list.append(guess)
+            break
+    return(guess)
+def determind(guess,answer):
+    global t_count
     if guess in answer:
         index=0
         for i in answer:
@@ -21,12 +28,37 @@ while True:
               t_count+=1
           index+=1
         print(show_answer)
-        if t_count==len(answer):
+def final(t_count,answer):
+    global count
+    global con
+    if t_count==len(answer):
             print("you get it with %d error only"%count)
-            break
+            con = "F"
     else:
         print("wrong guess")
         count+=1
         if count >=9:
              print("you die")
-             break
+
+
+
+
+
+
+
+import random
+guess_list=[]
+answer_list=["apple","arrange","quiet","op","pop","cute"]
+answer = ran_answer(answer_list)
+print(answer)
+print("there are %d letter in the word"%len(answer))
+show_answer=create_show_answer(answer)
+print(show_answer)
+count=0
+t_count=0
+con = "T"
+"T"
+while con=="T":
+    guess=get_guess()
+    determind(guess,answer) 
+    final(t_count,answer)
